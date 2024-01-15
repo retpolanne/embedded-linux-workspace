a003c1582158d2cdabe0c3d4a2ce6997f9ae5a62
Anne Macedo <retpolanne@posteo.net>
2024-01-15 14:41:10 +0000

    More configs and stuff

--- setup.sh
+++ setup.sh
@@ -5,4 +5,10 @@ sudo pacman -S - < pacman.txt
 echo "Installing crosstool"
 git submodule sync --recursive
 echo "Bootstrapping crosstool"
-cd crosstool-ng && ./bootstrap
+ls crosstool-ng/ct-ng || { cd crosstool-ng && ./bootstrap && ./configure --enable-local && make; }
+ln -fns $PWD/ct-ng-config $PWD/crosstool-ng/.config
+echo "Installing yay"
+yay || { git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si; }
+yay -S - < yay.txt
+echo "Configuring pwclient"
+ln -fns $PWD/pwclientrc $HOME/.pwclientrc
